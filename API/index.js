a5dbddbda2dd7e7cb213df946aee60f9785a4b1b
SandyFachriansyah <fachriansyahsandy@gmail.com>
2023-10-07 01:08:57 +0700

    connect to database

--- API/index.js
+++ API/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import moongoose from 'moongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+moongoose.connect(process.env.MONGO).then(() => {
+    console.log('conneted to Mongodb')
+    }).catch((err)=> {
+    console.log(err)
+    })
+
 
 const app = express();
 
