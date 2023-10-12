ab047a3452d25a9fe16b1735ee5f2d45f028edb8
SandyFachriansyah <fachriansyahsandy@gmail.com>
2023-10-12 09:07:01 +0700

    compleate image functionality

--- API/models/user.model.js
+++ API/models/user.model.js
@@ -21,6 +21,6 @@ const userSchema = new mongoose.Schema({
     },
 }, {timestamps: true})
 
-const user = mongoose.model('user', userSchema);
+const user = mongoose.model('User', userSchema);
 
 export default user;
