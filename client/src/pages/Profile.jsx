49276894da5f58d0464484f72768587a53dad160
SandyFachriansyah <fachriansyahsandy@gmail.com>
2023-10-18 07:57:26 +0700

    add delete user functionally

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,9 @@ import {
 } from 'firebase/storage';
 import { app } from '../firebase.js';
 import { 
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   updateUserFailure, 
   updateUserStart, 
   updateUserSucces 
@@ -79,9 +82,28 @@ export default function Profile() {
         dispatch(updateUserFailure(data.message))
         return
       }
-      dispatch(updateUserSucces(data))
+      dispatch(updateUserSucces(data));
+      setUpdateSuccess(true)
     } catch (error) {
       dispatch(updateUserFailure(error.message))
+    }
+  }
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch (deleteUserStart())
+      const res = await fetch (`/api/user/delete/${currentUser._id}`,{
+        method : 'DELETE',
+      })
+      const data = await res.json()
+      if (data.success === false){
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch (deleteUserFailure(error.message))
+      
     }
   }
   return (
@@ -142,14 +164,14 @@ export default function Profile() {
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
 
       <p className='text-red-700 mt-5'>
         {error ? error : ''}
       </p>
-      <p className='text-red-700 mt-5'>
+      <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is Updated successfully' : ''}
       </p>
     </div>
